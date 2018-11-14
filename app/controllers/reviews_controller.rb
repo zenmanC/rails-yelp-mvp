d1125f302d8600d653420f599350da8f4ccaa3a1
Chris Cheung <ckwcheung123@icloud.com>
2018-11-14 16:07:15 +0800

    now fixed

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -15,8 +15,13 @@ class ReviewsController < ApplicationController
 
   def create
     @review = Review.new(review_params)
-    @review.restaurant = Restaurant.find(params[:restaurant_id])
-    @review.save
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
 private
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
-  belongs_to :restaurants
+  belongs_to :restaurant
   validates :content, presence: true
   validates :rating, presence: true
   validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }
